977f3a50571e3805f2aa95665433508cfb2435bd
zieren <github@zieren.de>
2018-12-19 16:18:33 +0100

    Remove unused/debug code.

--- background.js
+++ background.js
@@ -1,6 +1,4 @@
-var defaultEnabled = true;  // XXX read from storage
 var processMessage = function(message, sender, sendResponse) {
-  console.log(message);
   if (message.operation == 'setIconOn') {
     browser.pageAction.setIcon({tabId: sender.tab.id, path: 'icons/icon-on.svg'});
   } else if (message.operation == 'setIconOff') {
