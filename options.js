4b244640b210eefc9165a302e67746f815ff7497
zieren <github@zieren.de>
2018-12-19 19:07:43 +0100

    Remove debug code.

--- options.js
+++ options.js
@@ -1,18 +1,10 @@
 var saveOptions = function(event) {
   event.preventDefault();
   browser.storage.sync.set({defaultEnabled: document.getElementById('defaultEnabled').checked});
-//  alert('set to: ' + document.getElementById('defaultEnabled').checked);
 };
 
 var restoreOptions = function(event) {
   browser.storage.sync.get('defaultEnabled').then(result => {
-//    alert('value: ' + result.defaultEnabled);
-//    alert('typeof: ' + typeof result.defaultEnabled);
-//    if (enabled === null) {
-//      enabled = true;
-//    } else {
-//      enabled = enabled == 'true';
-//    }
     document.getElementById('defaultEnabled').checked = result.defaultEnabled;
   });
 };
