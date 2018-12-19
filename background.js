8e0a3b7bb4eabdf68920e49cb78ecdd8a8578734
zieren <github@zieren.de>
2018-12-19 14:04:33 +0100

    More files.

---
+++ background.js
@@ -0,0 +1,18 @@
+var defaultEnabled = true;  // XXX read from storage
+var processMessage = function(message, sender, sendResponse) {
+  console.log('XXX background received:');
+  console.log(message);
+  if (message.operation == 'getDefaultEnabled') {  // XXX unused!?
+    console.log('XXX background sending default enabled: ' + defaultEnabled);
+    sendResponse({defaultEnabled: defaultEnabled});
+  } else if (message.operation == 'setIconOn') {
+    browser.pageAction.setIcon({tabId: sender.tab.id, path: 'icons/icon-on.svg'});
+  } else if (message.operation == 'setIconOff') {
+    browser.pageAction.setIcon({tabId: sender.tab.id, path: 'icons/icon-off.svg'});
+  }
+}
+browser.runtime.onMessage.addListener(processMessage);
+
+browser.pageAction.onClicked.addListener(tab => {
+  browser.tabs.sendMessage(tab.id, {operation: 'iconClicked'});
+});
