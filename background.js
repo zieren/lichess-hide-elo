3a09e352b9cb564de2e9184c2bd261406c0285f6
zieren <github@zieren.de>
2018-12-22 22:21:27 +0100

    Cosmetics.

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
-var processMessage = function(message, sender, sendResponse) {
+function processMessage(message, sender, sendResponse) {
   if (message.operation == 'setIconOn') {
     browser.pageAction.setIcon({tabId: sender.tab.id, path: 'icons/icon-on.svg'});
   } else if (message.operation == 'setIconOff') {
     browser.pageAction.setIcon({tabId: sender.tab.id, path: 'icons/icon-off.svg'});
   }
-};
+}
 browser.runtime.onMessage.addListener(processMessage);
 
 browser.pageAction.onClicked.addListener(tab => {
